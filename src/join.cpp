3e10533b78dc87a6b658334eb23cb3dffec5aeb4
Mark Ono <60458022+the-mark-algorithm@users.noreply.github.com>
2023-04-28 19:02:27 -0500

    Final BNLJ join implementation, all tests passing!

--- src/join.cpp
+++ src/join.cpp
@@ -1,51 +1,101 @@
 #include "join.hpp"
 
-#include <array>
 #include <cstdint>
 #include <vector>
+#include <iostream>
 
-JoinAlgorithm getJoinAlgorithm() {
-  // TODO: Return your chosen algorithm.
-  // return JOIN_ALGORITHM_BNLJ;
-  // return JOIN_ALGORITHM_SMJ;
-  // return JOIN_ALGORITHM_HJ;
-  throw std::runtime_error("not implemented: getJoinAlgorithm");
+JoinAlgorithm getJoinAlgorithm()
+{
+  return JOIN_ALGORITHM_BNLJ;
 };
 
 int join(File &file, int numPagesR, int numPagesS, char *buffer,
-         int numFrames) {
-  // TODO: Implement your chosen algorithm.
-  // Currently, this function performs a nested loop join in memory. While it
-  // will pass the correctness and I/O cost tests, it ignores the provided
-  // buffer and instead reads table data into vectors. It will not satisfy the
-  // constraints on peak heap memory usage for large input tables. You should
-  // delete this code and replace it with your disk-based join algorithm
-  // implementation.
-
-  int pageIndexR = 0;
-  int pageIndexS = pageIndexR + numPagesR;
-  int pageIndexOut = pageIndexS + numPagesS;
-
-  std::vector<Tuple> tuplesR(numPagesR * 512);
-  file.read(tuplesR.data(), pageIndexR, numPagesR);
-
-  std::vector<Tuple> tuplesS(numPagesS * 512);
-  file.read(tuplesS.data(), pageIndexS, numPagesS);
+         int numFrames)
+{
+  
+  int pageIndexR = 0;                         //initial page index for R
+  int pageIndexS = pageIndexR + numPagesR;    //initial page index for S
+  int pageIndexOut = pageIndexS + numPagesS;  //initial page index for tuplesOut
+  int numPagesRUnChecked = numPagesR;
+  int internalPageIndexS = pageIndexS;        //internal page index for S to iterate through R pages
+  int internalPageIndexR = pageIndexR;        //internal page index for R to iterate through S pages
+  int internalPageIndexOut = pageIndexOut;
+  int numCurrentPagesLeft = 0;
+  int numTuplesOut = 0;
 
   std::vector<Tuple> tuplesOut;
+  Tuple *tuplesR;
+  Tuple *tuplesS;
 
-  for (const Tuple &tupleR : tuplesR) {
-    for (const Tuple &tupleS : tuplesS) {
-      if (tupleR.first == tupleS.first) {
-        tuplesOut.emplace_back(tupleR.second, tupleS.second);
-        break;
+  void *ptr_at_S;
+
+  u_int32_t sFirst;
+  u_int32_t sSecond;
+  u_int32_t rFirst;
+  u_int32_t rSecond;
+
+  while (numPagesRUnChecked != 0)
+  {
+    if (numPagesRUnChecked < numFrames - 2)
+    { // only one page left to check
+      numCurrentPagesLeft = numPagesRUnChecked;
+    }
+    else // more than one pages left to check
+    {
+      numCurrentPagesLeft = numFrames - 2; 
+    }
+
+    // same for either case
+    file.read(buffer, internalPageIndexR, numCurrentPagesLeft);
+    tuplesR = (Tuple *)buffer;
+
+    ptr_at_S = (void *)(buffer + (numCurrentPagesLeft) * 512 * 8);
+    internalPageIndexS = pageIndexS;
+    for (int i = 0; i < numPagesS; i++)
+    {
+      file.read((char *)ptr_at_S, internalPageIndexS, 1);
+      tuplesS = (Tuple *)ptr_at_S;
+      
+      for (int s = 0; s < 512; s++)
+      {
+        sFirst = std::get<0>(tuplesS[s]);
+        sSecond = std::get<1>(tuplesS[s]);
+        for (int r = 0; r  < 512 * numCurrentPagesLeft; r++)
+        {
+          rFirst = std::get<0>(tuplesR[r]);
+          rSecond = std::get<1>(tuplesR[r]);
+
+          if (rFirst == sFirst and rFirst > 0) {
+            tuplesOut.emplace_back(rSecond, sSecond);
+            numTuplesOut += 1;
+            if ((numTuplesOut % 512) == 0)
+            {
+              file.write(tuplesOut.data(), internalPageIndexOut, 1);
+              internalPageIndexOut += 1;
+              tuplesOut.clear();
+            }
+            break;
+          }
+        }
       }
+        internalPageIndexS += 1;
+    }
+
+    if (numPagesRUnChecked < numFrames - 2)
+    {
+      numPagesRUnChecked -= numCurrentPagesLeft;
+    }
+    else
+    {
+      numPagesRUnChecked -= numCurrentPagesLeft;
+      internalPageIndexR += numCurrentPagesLeft;
     }
   }
 
-  int numTuplesOut = (int)tuplesOut.size();
   int numPagesOut = numTuplesOut / 512 + (numTuplesOut % 512 != 0);
-  file.write(tuplesOut.data(), pageIndexOut, numPagesOut);
+  file.write(tuplesOut.data(), internalPageIndexOut, 1);
+  tuplesOut.clear();
 
   return numTuplesOut;
 }
+
